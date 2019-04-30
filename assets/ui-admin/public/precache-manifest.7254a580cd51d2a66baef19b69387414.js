5566ba772d266c2ae5352a72e25108154eeab4be
heena <2015heena.dawani@ves.ac.in>
2019-04-30 20:38:14 +0530

    assets

---
+++ assets/ui-admin/public/precache-manifest.7254a580cd51d2a66baef19b69387414.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb3326647359f6139ec1ade72a9bad39",
+    "url": "/admin/static/media/loading-circle.eb332664.svg"
+  },
+  {
+    "revision": "3f82f5bf8566dc34c9b5",
+    "url": "/admin/static/js/runtime~main.3f82f5bf.js"
+  },
+  {
+    "revision": "d5a3369f4852503d3cfe",
+    "url": "/admin/static/js/main.d5a3369f.chunk.js"
+  },
+  {
+    "revision": "e9d0c94e1bb0127212bc",
+    "url": "/admin/static/js/1.e9d0c94e.chunk.js"
+  },
+  {
+    "revision": "d5a3369f4852503d3cfe",
+    "url": "/admin/static/css/main.3fd5b1df.chunk.css"
+  },
+  {
+    "revision": "e9d0c94e1bb0127212bc",
+    "url": "/admin/static/css/1.5db1dc64.chunk.css"
+  },
+  {
+    "revision": "1e096bed3f759a4eb8d7a1172476dbe1",
+    "url": "/admin/index.html"
+  }
+];
